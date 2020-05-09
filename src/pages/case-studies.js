7cb8b1b937c7d16d9532b0cdb38fc3eb4d4a74c2
Rahul Sekhar <sekhar.rahul@gmail.com>
2020-05-08 19:54:33 -0700

    List case studies in parent page

--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -1,13 +1,47 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const CaseStudies = () => (
+const CaseStudyLink = ({ title, path }) => <Link to={path}>{title}</Link>
+
+const CaseStudies = ({ data }) => (
   <Layout>
     <SEO title="Case Studies" />
     <h2>Case studies</h2>
+
+    <ol>
+      {data.allMdx.edges.map(({ node }) => (
+        <li key={node.id}>
+          <CaseStudyLink
+            title={node.frontmatter.title}
+            path={`case-studies${node.fields.slug}`}
+          />
+        </li>
+      ))}
+    </ol>
   </Layout>
 )
 
 export default CaseStudies
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
