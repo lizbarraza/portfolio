d19de05d5d89175a93dd34260da51c3dfb5b3381
Rahul Sekhar <sekhar.rahul@gmail.com>
2020-05-08 18:44:54 -0700

    Add SEO element for case studies

--- src/templates/case-study.js
+++ src/templates/case-study.js
@@ -1,10 +1,13 @@
 import React from "react"
-import Layout from "../components/layout"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function CaseStudy({ data }) {
   return (
     <Layout>
+      <SEO title={data.mdx.frontmatter.title} />
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   )
@@ -14,6 +17,9 @@ export const query = graphql`
   query($slug: String!) {
     mdx(fields: { slug: { eq: $slug } }) {
       body
+      frontmatter {
+        title
+      }
     }
   }
 `
