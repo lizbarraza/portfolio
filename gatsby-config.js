c5c5d14ae9ecc3b8867f9628f10ff2bed1ac9786
Rahul Sekhar <sekhar.rahul@gmail.com>
2020-07-29 13:52:05 -0700

    Add google fonts, case study header

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,21 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`700`],
+          },
+          {
+            family: `Noto Serif`,
+            variants: [`400`],
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/elements/case-study-header/case-study-header.js
+++ src/components/elements/case-study-header/case-study-header.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 
 import styles from "./case-study-header.module.css"
 
-export const CaseStudyHeader = (title, subtitle, backgroundColor) => {
+export const CaseStudyHeader = ({ title, subtitle, backgroundColor }) => {
   return (
     <header className={styles.caseStudyHeader} style={{ backgroundColor }}>
       <h1 className={styles.caseStudyTitle}>{title}</h1>
