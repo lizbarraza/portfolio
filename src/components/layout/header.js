2eefb62531547d9b86f6e806560be82862c508af
Rahul Sekhar <sekhar.rahul@gmail.com>
2020-07-29 12:36:51 -0700

    Add temporary header styles

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -3,6 +3,8 @@ import { Link } from "gatsby"
 
 import { useStaticQuery, graphql } from "gatsby"
 
+import styles from "./header.module.css"
+
 export const Header = () => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -15,10 +17,10 @@ export const Header = () => {
   `)
 
   return (
-    <header>
-      <h1>{data.site.siteMetadata.title}</h1>
+    <header className={styles.header}>
+      <h1 className={styles.title}>{data.site.siteMetadata.title}</h1>
 
-      <nav>
+      <nav className={styles.menu}>
         <ul>
           <li>
             <Link to="/case-studies/">Work</Link>
