9f22a94ea75bd4a80d0d0264f006f883df8f1caf
Rahul Sekhar <sekhar.rahul@gmail.com>
2020-08-28 17:52:31 -0700

    Add laptop frame for scrollable-image

--- src/components/elements/scrollable-image/scrollable-image.js
+++ src/components/elements/scrollable-image/scrollable-image.js
@@ -1,13 +1,21 @@
 import React from "react"
 import Img from "gatsby-image"
 import { sharpFluidResultToGatsbyImage } from "../../../lib"
+import laptopFrameSvg from "../../../images/laptop-frame.svg"
 
 import styles from "./scrollable-image.module.css"
 
 export const ScrollableImage = props => {
   return (
     <div className={styles.scrollableImage}>
-      <Img fluid={sharpFluidResultToGatsbyImage(props)} />
+      <img
+        alt=""
+        className={styles.scrollableImageFrame}
+        src={laptopFrameSvg}
+      />
+      <div class={styles.scrollableImageInner}>
+        <Img fluid={sharpFluidResultToGatsbyImage(props)} />
+      </div>
     </div>
   )
 }
